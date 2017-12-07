cdcf8e3ee9dc89c7eb20deebd075bea33c747e8e
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-07 14:28:17 -0700

    Added AddItem Function to Shopping List

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -1,16 +1,48 @@
 'use strict';
 
 function addItem() {
+  $('#js-shopping-list-form').submit(function(event) {
+
+    event.preventDefault();
   
+    let userText = $(this).find('.js-shopping-list-entry').val();
+    console.log(userText);
+
+    $('ul').append(
+      `<li>
+      <span class="shopping-item">${userText}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`
+    );
+
+  });
 }
 
+function checkItem() {
+  $('.shopping-item-controls').on('click', (function(event) {
+    //When check button is clicked, add strikethrough
+    //class to span class 'shopping-item'
+    // console.log($(event.currentTarget).parent());
 
+    $('.shopping-item').addClass('.shopping-item__checked');
+      
+  }));
+}
+  
 
-addItem();
+$(addItem);
+$(checkItem);
 
   
-// $(function() {
-//   $('body').css('background-color', 'red');
-// });
-  
 
+
+// append a div to the end of the list
+// append to <ul class="shopping-list">
+  
