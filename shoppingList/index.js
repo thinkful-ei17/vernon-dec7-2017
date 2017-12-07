1ab2da78acadbd4f499fd21a92884298562c7699
Vernon Mensah <vernonmensah@gmail.com>
2017-12-07 18:01:02 -0500

    shopplist with all the needed functions.
    go to hell jquery. kidding.

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -4,7 +4,7 @@ function addItem() {
   $('#js-shopping-list-form').submit(function(event) {
 
     event.preventDefault();
-  
+
     let userText = $(this).find('.js-shopping-list-entry').val();
     console.log(userText);
 
@@ -25,24 +25,56 @@ function addItem() {
   });
 }
 
-function checkItem() {
-  $('.shopping-item-controls').on('click', (function(event) {
-    //When check button is clicked, add strikethrough
-    //class to span class 'shopping-item'
-    // console.log($(event.currentTarget).parent());
 
-    $('.shopping-item').addClass('.shopping-item__checked');
-      
-  }));
+let ifCheckButton;
+let ifCheckWord;
+let ifDeleteButton;
+let ifDeleteWord;
+
+function checkItem() {
+ //attaching a listener to ul since
+ //if all of li items have one, but newly created ones dont
+
+  $("ul").on("click", "li", function(event) {
+    //When check button is clicked, add strikethrough
+    //when delete button is clicked, delete li!
+
+    //if user clicks check button
+      //do this
+   //if other button
+      //do this
+
+    //variables to check whether or not elements specific to a button
+    //were clicked
+    ifCheckButton = $(event.target).hasClass("shopping-item-toggle") ;
+    ifCheckWord = event.target.outerText === "check" ? true : false;
+
+    ifDeleteButton = $(event.target).hasClass("shopping-item-delete");
+    ifDeleteWord = event.target.outerText === "delete" ? true : false;
+
+    //since we a listener is attaced to all things in li
+    //we listen for specific events/items that were clicked
+    // a button can have its label and outerbutton clicked
+    if (ifCheckButton || ifCheckWord) {
+      console.log("BAM");
+      $(this).find(".shopping-item").toggleClass("shopping-item__checked");
+    }
+
+    if (ifDeleteButton || ifDeleteWord) {
+      console.log("BOOM");
+      $(this).remove();
+    }
+
+  });
+
 }
-  
+
 
 $(addItem);
 $(checkItem);
 
-  
+
 
 
 // append a div to the end of the list
 // append to <ul class="shopping-list">
-  
