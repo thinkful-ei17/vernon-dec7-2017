f31e9fe4ceece77ea2fc4a68532a7a882884f9f8
Vernon Mensah <vernonmensah@gmail.com>
2017-12-07 13:10:49 -0500

    Added functionality for tab-tab-enter nav

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -3,15 +3,33 @@
 //find out which one was clicked
 //replace item in .hero element
 //
-$(".thumbnails").click(function(item) {
+$(".thumbnails").on("click", function (item) {
 
-  console.log(item.target.src);
-  let newImg = item.target;
-  let previousImage = $(".hero img");
+  const clickedNavImg = item.target;
+  const accessibleNavImg = $(item.target).find("img")[0];
 
-  previousImage.attr({
-    src: item.target.src,
-    alt: item.target.alt
-  });
+  const previousImage = $(".hero img");
 
-});
+  //accessing the img through tab-tab-enter aka accessibleNav
+  //gives us <a><img> instead of just <img>
+  //line 9 retrieves img from <a><img><img></a>. find() gives an obj. obj[0] gives me the html.
+  //so calling/accessing accessibleNavImg when user clicks will return undefined - img couldnt be found in img element lol
+  //      -> changes img with clickedimg
+  //calling/accessing accessibleNavImg when user tab-tab-enter will return the image - img is found in <a elemen
+ //      -> changes img with clickedimg
+  if (!accessibleNavImg){
+
+    previousImage.attr({
+      src: clickedNavImg.src,
+      alt: clickedNavImg.alt
+    });
+  }
+
+  else {
+    previousImage.attr({
+      src: accessibleNavImg.src,
+      alt: accessibleNavImg.alt
+    });
+  }
+
+})
