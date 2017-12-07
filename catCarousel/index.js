d3a03cd4ce41b6e5811dc13a138d9048454bf99d
Vernon Mensah <vernonmensah@gmail.com>
2017-12-07 15:01:07 -0500

    Refactored
    Added comments to explain changes.

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -3,34 +3,20 @@
 //find out which one was clicked
 //replace item in .hero element
 //
-$(".thumbnails").on("click", function (item) {
+$(".thumbnail").on("click", function (item) {
+//args --> this === event.currentTarget !== event.target
+//$(args) to use jquery functionality/lib
+//tests to understand what the Event object is.
+  // console.log(event.currentTarget);
+  // console.log("====");
+  // console.log(event.target);
+  // console.log("====");
+  // console.log(event);
 
-  const clickedNavImg = item.target;
-  const accessibleNavImg = $(item.target).find("img")[0];
 
-  const previousImage = $(".hero img");
-
-  //accessing the img through tab-tab-enter aka accessibleNav
-  //gives us <a><img> instead of just <img>
-  //line 9 retrieves img from <a><img><img></a>. find() gives an obj. obj[0] gives me the html.
-  //so calling/accessing accessibleNavImg when user clicks will return undefined - img couldnt be found in img element lol
-  //      -> changes img with clickedimg
-  //calling/accessing accessibleNavImg when user tab-tab-enter will return the image - img is found in <a elemen
- //      -> changes img with clickedimg
- console.log(clickedNavImg);
-  if (!accessibleNavImg){
-
-    previousImage.attr({
-      src: clickedNavImg.src,
-      alt: clickedNavImg.alt
+  $(".hero img").previousImage.attr({
+      src: $(event.currentTarget).find("img").attr('src'),
+      alt: $(event.currentTarget).find("img").attr('alt')
     });
-  }
-
-  else {
-    previousImage.attr({
-      src: accessibleNavImg.src,
-      alt: accessibleNavImg.alt
-    });
-  }
 
 })
